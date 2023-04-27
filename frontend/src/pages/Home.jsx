f3a5eb67e39dbd16d4c5d04b06057cf5db420b3a
banma1234 <banma1234@gmail.com>
2023-04-27 18:02:23 +0900

    build: default setting

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <h1>HOME</h1>
+      <Link to={"/about"}>이동</Link>
+    </>
+  );
+}
