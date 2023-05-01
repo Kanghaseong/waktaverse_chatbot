dc88454f9441471b65f334349df9b7205f8048e1
Kanghaseong <hskang200@gmail.com>
2023-05-01 20:20:47 +0900

    test version on

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,42 @@
 require("dotenv").config();
 const express = require("express");
+const fs = require("fs");
 const app = express();
-const port = 3000;
+const port = 4000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json())
+
 const { Configuration, OpenAIApi } = require("openai");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
 const openai = new OpenAIApi(configuration);
 
-app.get("/", async (req, res) => {
-    const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: "Hello world" }],
-      });
-      console.log(completion.data.choices[0].message);
-  res.send("Hello World!");
+let Gosegu = "";
+
+fs.readFile("goseguText.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  Gosegu = data;
+});
+
+app.post("/chat", async (req, res) => {
+  const inputValue = req.body.inputValue;
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: [
+      { role: "system", content: Gosegu },
+      { role: "user", content: `${inputValue}` },
+    ],
+  });
+  console.log(completion.data.choices[0].message.content);
+
+  res.send(completion.data.choices[0].message.content);
 });
 
 app.listen(port, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,45 @@
-import Router from "./router/router.js";
+import React, { useState } from "react";
+import axios from "axios";
+import "./index.css";
 
 function App() {
+  const [texts, setTexts] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleButtonClick = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    setIsLoading(true); // Set isLoading to true
+    axios.post("http://localhost:4000/chat", { inputValue })
+      .then(response => {
+        setTexts([...texts, response.data]);
+        setInputValue("");
+      })
+      .catch(error => console.error(error))
+      .finally(() => setIsLoading(false)); // Set isLoading to false when request is finished
+  };
+
   return (
-    <>
-      <div className="App">
-        <Router />
-      </div>
-    </>
+    <div className="App">
+      <h1>고세구 GPT</h1>
+      <form onSubmit={handleButtonClick}>
+        <label>
+          고세구 한테 할 말을 입력하세요 
+          <input
+            type="text"
+            value={inputValue}
+            onChange={event => setInputValue(event.target.value)}
+          />
+        </label>
+        <button type="submit">전송</button>
+      </form>
+      {isLoading && <p>Loading...</p>}
+      <ul>
+        {texts.map((text, index) => (
+          <li key={index}>{text}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
